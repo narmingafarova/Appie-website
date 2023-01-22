2c2596bf9b7ffc6659050402c61a84b09aeb9666
narmingafarova <qafarovanermin72@gmail.com>
2023-01-22 15:38:42 +0400

    Language change and local storage up to the authors

--- js/data/language.js
+++ js/data/language.js
@@ -1,24 +1,155 @@
 const langData = {
+  //! Eng Language Data
+  //* Nav and Initial Section Eng
+  nav_link_en: ["Home", "About Us", "Pages", "Mega Menu", "Blog", "Contact"],
+  nav_dropdown_en: [
+    "About Us 2",
+    "Service",
+    "Service 2",
+    "Service 3",
+    "Service Details",
+    "Service Details 2",
+    "Shop Left Sidebar",
+    "Shop Right Sidebar",
+    "Shop Full Width",
+    "Blog",
+    "Blog Details",
+    "Blog Details 2",
+    "Blog Details 3",
+  ],
+  nav_btn_en: ['<i class="fa-regular fa-user pe-2"></i>Login', "Get Started"],
+  init_lang_en: [
+    "Welcome To Appie",
+    "Manage it all, in this all new system.",
+    "Hanky panky lavatory up the duff jolly good cack brolly is chinwag zonked happy days sloshed.",
+    '<i class="fa-brands fa-apple"></i>Download for IOS',
+    '<i class="fa-brands fa-google-play"></i>Download for Android',
+  ],
+
+  //* Design Section Eng
+  destitle_h2_en: ["Designed with <br/> The applicant in mind."],
+  destitle_p_en: [
+    "The full monty spiffing good time no biggie cack grub fantastic.",
+  ],
+  descardtitle_en: ["Searching", "Designing", "Building", "Supporting"],
+  descardtext_en: [
+    "Mucker plastered bugger all mate morish are.",
+    "Mucker plastered bugger all mate morish are.",
+    "Mucker plastered bugger all mate morish are.",
+    "Mucker plastered bugger all mate morish are.",
+  ],
+
+  //* Reactions Eng
+  reac_list_en: ["Settings", "Report", "Safety Notices", "App Lock"],
+  reac_info_en: [
+    "Custom Reactions",
+    "Higlight",
+    "the app features",
+    "Car boot absolutely bladdered posh burke the wireless mush some dodg.",
+    "Learn more",
+  ],
+
+  //* Traffic Eng
+  traf_info_en: [
+    "Traffic",
+    "Stage reporting with 50k queries.",
+    "He nicked it tickety boo harry the cras bargy chap mush spiffing spend a penny the full monty burke butty.",
+    "Learn more <i class='fas fa-arrow-right'></i>",
+  ],
+  traf_list_title_en: [
+    "Carefully designed",
+    "Choose a App",
+    "Seamless Sync",
+    "User Interactive",
+  ],
+  traf_list_text_en: [
+    "Mucker plastered bugger all mate morish are.",
+    "Mucker plastered bugger all mate morish are.",
+    "Mucker plastered bugger all mate morish are.",
+    "Mucker plastered bugger all mate morish are.",
+  ],
+
+  //! Az Language Data
+  //* Nav and Initial Section Az
   nav_link_az: [
     "Ana Səhifə",
-    "Haqqimizda",
+    "Haqqımızda",
     "Səhifələr",
     "Mega Menyu",
     "Bloq",
     "Əlaqə",
   ],
-  nav_link_en: ['Home', 'About Us', 'Pages', 'Mega Menu', 'Blog', 'Contact'],
+  nav_dropdown_az: [
+    "Haqqımızda 2",
+    "Xidmət",
+    "Xidmət 2",
+    "Xidmət 3",
+    "Xidmət təfərrüatları",
+    "Xidmət Təfərrüatları 2",
+    "Mağaza Sol Yan Panel",
+    "Mağazanın Sağ Yan Paneli",
+    "Mağazanın tam eni",
+    "Bloq",
+    "Bloq təfərrüatları",
+    "Bloq Təfərrüatları 2",
+    "Bloq Təfərrüatları 3",
+  ],
   nav_btn_az: ['<i class="fa-regular fa-user pe-2"></i>Daxil ol', "Başla"],
-  nav_btn_en: ['<i class="fa-regular fa-user pe-2"></i>Login', "Get Started"],
   init_lang_az: [
     "Appie xoş gəlmisiniz",
     "Bu yeni sistemdə hər şeyi idarə edin.",
-    "Hanky panky tualet qədər duff jolly yaxşi cack brolly chinwag zonked xoşbəxt gün sloshed edir.",
+    "Hanky panky tualet qədər duff jolly yaxşı cack brolly chinwag zonked xoşbəxt gün sloshed edir.",
     '<i class="fa-brands fa-apple"></i>IOS üçün yükləyin',
     '<i class="fa-brands fa-google-play"></i>Android üçün yükləyin',
   ],
-  init_lang_en: ['Welcome To Appie', 'Manage it all, in this all new system.', 'Hanky panky lavatory up the duff jolly good cack brolly is chinwag zonked happy days sloshed.', '<i class="fa-brands fa-apple"></i>Download for IOS', '<i class="fa-brands fa-google-play"></i>Download for Android']
 
+  //* Design Section Az
+  destitle_h2_az: ["Ərizəçi nəzərə <br/> alınmaqla hazırlanmışdır."],
+  destitle_p_az: [
+    "Tam monty spiffing yaxşı vaxt heç bir biggie cack grub fantastik.",
+  ],
+  descardtitle_az: ["Axtarış", "Layihələndirmə", "Tikinti", "Dəstək"],
+  descardtext_az: [
+    "Mucker suvaqlı bugger bütün mate morish var.",
+    "Mucker suvaqlı bugger bütün mate morish var.",
+    "Mucker suvaqlı bugger bütün mate morish var.",
+    "Mucker suvaqlı bugger bütün mate morish var.",
+  ],
+
+  //* Reactions Az
+  reac_list_az: [
+    "Parametrlər",
+    "Hesabat",
+    "Təhlükəsizlik Bildirişləri",
+    "Tətbiq Kilidi",
+  ],
+  reac_info_az: [
+    "Xüsusi reaksiyalar",
+    "Tətbiq xüsusiyyətlərini",
+    "vurğulamaq",
+    "Avtomobilin baqajı tamamilə qovuq ilə fərqlənir.",
+    "Daha ətraflı",
+  ],
+
+  //* Traffic Az
+  traf_info_az: [
+    "Yol hərəkəti",
+    "50k sorğu ilə Mərhələ <br/> hesabatı.",
+    "O, nicked it tickety boo Harry the cras bargy chap mush spiffing spiffing the full monty burke butty.",
+    "Daha ətraflı <i class='fas fa-arrow-right'></i>",
+  ],
+  traf_list_title_az: [
+    "Diqqətlə hazırlanmışdır",
+    "Tətbiq seçin",
+    "Sorunsuz Sinxronizasiya",
+    "İstifadəçi İnteraktivi",
+  ],
+  traf_list_text_az: [
+    "Mucker suvaqlı bugger bütün mate morish var.",
+    "Mucker suvaqlı bugger bütün mate morish var.",
+    "Mucker suvaqlı bugger bütün mate morish var.",
+    "Mucker suvaqlı bugger bütün mate morish var.",
+  ],
 };
 
 export { langData };
--- js/script.js
+++ js/script.js
@@ -21,11 +21,30 @@ const modeBtn = document.querySelector(".mode-btn");
 // Nav
 const navLink = document.querySelectorAll(".nav-link");
 const navBtn = document.querySelectorAll(".nav-btn-lang");
+const dropdownItem = document.querySelectorAll(".dropdown-lang");
+const dropdownItemSpan = document.querySelector(".dropdown-item span");
 
 // Initial page
 const initLang = document.querySelectorAll(".init-lang");
 
-// Plans page
+// Design section
+const destitle_h2 = document.querySelector(".design .title h2");
+const destitle_p = document.querySelector(".design .title p");
+const descardtitle = document.querySelectorAll(".design .card .card-title");
+const descardtext = document.querySelectorAll(".design .card .card-text");
+
+// Reactions section
+const reactList = document.querySelectorAll(".reactions ul li a span");
+const reactInfo = document.querySelectorAll(".re_info_lang");
+
+// Traffic section
+const trafInfo = document.querySelectorAll(".traf-info-lan");
+const trafListTitle = document.querySelectorAll(".traffic-list .list-text h3");
+const trafListText = document.querySelectorAll(".traffic-list .list-text p");
+
+// Authors section
+
+// Plans section
 const input = document.getElementById("switch");
 const showcards = document.getElementsByClassName("show")[0];
 const plan_cards = document.getElementsByClassName("plancard-display")[0];
@@ -44,8 +63,13 @@ input.addEventListener("click", () => {
 // Language change
 langBtn.onclick = () => {
   if (langBtn.innerHTML === "AZ") {
+    // Nav and Initial sections
     for (let index in langData.nav_link_az) {
       navLink[index].innerHTML = langData.nav_link_az[index];
+    }
+    dropdownItemSpan.innerHTML = "Ev 1";
+    for (let index in langData.nav_dropdown_az) {
+      dropdownItem[index].innerHTML = langData.nav_dropdown_az[index];
     }
     for (let index in langData.nav_btn_az) {
       navBtn[index].innerHTML = langData.nav_btn_az[index];
@@ -54,10 +78,45 @@ langBtn.onclick = () => {
       initLang[index].innerHTML = langData.init_lang_az[index];
     }
 
+    // Design Section
+    destitle_h2.innerHTML = langData.destitle_h2_az;
+    destitle_p.innerHTML = langData.destitle_p_az;
+    for (let index in langData.descardtitle_az) {
+      descardtitle[index].innerHTML = langData.descardtitle_az[index];
+    }
+    for (let index in langData.descardtext_az) {
+      descardtext[index].innerHTML = langData.descardtext_az[index];
+    }
+
+    // React Section
+    for (let index in langData.reac_list_az) {
+      reactList[index].innerHTML = langData.reac_list_az[index];
+    }
+    for (let index in langData.reac_info_az) {
+      reactInfo[index].innerHTML = langData.reac_info_az[index];
+    }
+
+    // Traffic Section
+    for (let index in langData.traf_info_az) {
+      trafInfo[index].innerHTML = langData.traf_info_az[index];
+    }
+    for (let index in langData.traf_list_title_az) {
+      trafListTitle[index].innerHTML = langData.traf_list_title_az[index];
+    }
+    for (let index in langData.traf_list_text_az) {
+      trafListText[index].innerHTML = langData.traf_list_text_az[index];
+    }
+
     langBtn.innerHTML = "EN";
+    localStorage.setItem("lang", "az");
   } else {
+    // Nav and Initial sections
     for (let index in langData.nav_link_en) {
       navLink[index].innerHTML = langData.nav_link_en[index];
+    }
+    dropdownItemSpan.innerHTML = "Home 1";
+    for (let index in langData.nav_dropdown_en) {
+      dropdownItem[index].innerHTML = langData.nav_dropdown_en[index];
     }
     for (let index in langData.nav_btn_en) {
       navBtn[index].innerHTML = langData.nav_btn_en[index];
@@ -65,13 +124,187 @@ langBtn.onclick = () => {
     for (let index in langData.init_lang_en) {
       initLang[index].innerHTML = langData.init_lang_en[index];
     }
+
+    // Design Section
+    destitle_h2.innerHTML = langData.destitle_h2_en;
+    destitle_p.innerHTML = langData.destitle_p_en;
+    for (let index in langData.descardtitle_en) {
+      descardtitle[index].innerHTML = langData.descardtitle_en[index];
+    }
+    for (let index in langData.descardtext_en) {
+      descardtext[index].innerHTML = langData.descardtext_en[index];
+    }
+
+    // React Section
+    for (let index in langData.reac_list_en) {
+      reactList[index].innerHTML = langData.reac_list_en[index];
+    }
+    for (let index in langData.reac_info_en) {
+      reactInfo[index].innerHTML = langData.reac_info_en[index];
+    }
+
+    // Traffic Section
+    for (let index in langData.traf_info_en) {
+      trafInfo[index].innerHTML = langData.traf_info_en[index];
+    }
+    for (let index in langData.traf_list_title_en) {
+      trafListTitle[index].innerHTML = langData.traf_list_title_en[index];
+    }
+    for (let index in langData.traf_list_text_en) {
+      trafListText[index].innerHTML = langData.traf_list_text_en[index];
+    }
+
+    // Btn and local
     langBtn.innerHTML = "AZ";
+    localStorage.setItem("lang", "en");
   }
 };
+
+//Language LocalStorage
+if (localStorage.getItem("lang") === null) {
+  localStorage.setItem("lang", "en");
+} else {
+  if (localStorage.getItem("lang") == "en") {
+    // Nav and Initial sections
+    for (let index in langData.nav_link_en) {
+      navLink[index].innerHTML = langData.nav_link_en[index];
+    }
+    dropdownItemSpan.innerHTML = "Home 1";
+    for (let index in langData.nav_dropdown_en) {
+      dropdownItem[index].innerHTML = langData.nav_dropdown_en[index];
+    }
+    for (let index in langData.nav_btn_en) {
+      navBtn[index].innerHTML = langData.nav_btn_en[index];
+    }
+    for (let index in langData.init_lang_en) {
+      initLang[index].innerHTML = langData.init_lang_en[index];
+    }
+
+    // Design section
+    destitle_h2.innerHTML = langData.destitle_h2_en;
+    destitle_p.innerHTML = langData.destitle_p_en;
+    for (let index in langData.descardtitle_en) {
+      descardtitle[index].innerHTML = langData.descardtitle_en[index];
+    }
+    for (let index in langData.descardtext_en) {
+      descardtext[index].innerHTML = langData.descardtext_en[index];
+    }
+
+    // React Section
+    for (let index in langData.reac_list_en) {
+      reactList[index].innerHTML = langData.reac_list_en[index];
+    }
+    for (let index in langData.reac_info_en) {
+      reactInfo[index].innerHTML = langData.reac_info_en[index];
+    }
+
+    // Traffic Section
+    for (let index in langData.traf_info_en) {
+      trafInfo[index].innerHTML = langData.traf_info_en[index];
+    }
+    for (let index in langData.traf_list_title_en) {
+      trafListTitle[index].innerHTML = langData.traf_list_title_en[index];
+    }
+    for (let index in langData.traf_list_text_en) {
+      trafListText[index].innerHTML = langData.traf_list_text_en[index];
+    }
+
+    // Btn
+    langBtn.innerHTML = "AZ";
+  } else {
+    // Nav and Initial sections
+    for (let index in langData.nav_link_az) {
+      navLink[index].innerHTML = langData.nav_link_az[index];
+    }
+    dropdownItemSpan.innerHTML = "Ev 1";
+    for (let index in langData.nav_dropdown_az) {
+      dropdownItem[index].innerHTML = langData.nav_dropdown_az[index];
+    }
+    for (let index in langData.nav_btn_az) {
+      navBtn[index].innerHTML = langData.nav_btn_az[index];
+    }
+    for (let index in langData.init_lang_az) {
+      initLang[index].innerHTML = langData.init_lang_az[index];
+    }
+
+    // Design Section
+    destitle_h2.innerHTML = langData.destitle_h2_az;
+    destitle_p.innerHTML = langData.destitle_p_az;
+    for (let index in langData.descardtitle_az) {
+      descardtitle[index].innerHTML = langData.descardtitle_az[index];
+    }
+    for (let index in langData.descardtext_az) {
+      descardtext[index].innerHTML = langData.descardtext_az[index];
+    }
+
+    // React Section
+    for (let index in langData.reac_list_az) {
+      reactList[index].innerHTML = langData.reac_list_az[index];
+    }
+    for (let index in langData.reac_info_az) {
+      reactInfo[index].innerHTML = langData.reac_info_az[index];
+    }
+
+    // Traffic Section
+    for (let index in langData.traf_info_az) {
+      trafInfo[index].innerHTML = langData.traf_info_az[index];
+    }
+    for (let index in langData.traf_list_title_az) {
+      trafListTitle[index].innerHTML = langData.traf_list_title_az[index];
+    }
+    for (let index in langData.traf_list_text_az) {
+      trafListText[index].innerHTML = langData.traf_list_text_az[index];
+    }
+
+    // Btn and local
+    langBtn.innerHTML = "EN";
+  }
+}
 
 // Mode change
 modeBtn.onclick = () => {
   if (body.className === "bg-white") {
+    body.className = "bg-dark";
+    nav.className = "navbar navbar-expand-lg bg-dark nav-dark py-3";
+    initial.className =
+      "initial init-dark d-flex justify-content-center flex-column";
+    design.className = "design bg-dark design-dark";
+    reactions.className = "reactions reactions-dark";
+    traffic.className = "traffic traffic-dark position-relative";
+    team.className = "team team-dark";
+    plans.className = "plans plans-dark bg-dark";
+    questions.className = "questions questions-dark bg-dark";
+    blogs.className = "blog blog-dark";
+    footer.className = "footer footer-dark bg-dark";
+    footBar.className = "foot-bar foot-bar-dark";
+    modeBtn.innerHTML = "🌞";
+
+    localStorage.setItem("mode", "dark");
+  } else {
+    body.className = "bg-white";
+    nav.className = "navbar navbar-expand-lg bg-white py-3";
+    initial.className = "initial d-flex justify-content-center flex-column";
+    design.className = "design";
+    reactions.className = "reactions";
+    traffic.className = "traffic position-relative";
+    team.className = "team";
+    plans.className = "plans";
+    questions.className = "questions";
+    blogs.className = "blog";
+    footer.className = "footer";
+    footBar.className = "foot-bar";
+    modeBtn.innerHTML = "🌙";
+
+    localStorage.setItem("mode", "light");
+  }
+  modeBtn.classList.toggle("mode-btn-dark");
+};
+
+//Mode LocalStorage
+if (localStorage.getItem("mode") === null) {
+  localStorage.setItem("mode", "light");
+} else {
+  if (localStorage.getItem("mode") == "dark") {
     body.className = "bg-dark";
     nav.className = "navbar navbar-expand-lg bg-dark nav-dark py-3";
     initial.className =
@@ -101,5 +334,4 @@ modeBtn.onclick = () => {
     footBar.className = "foot-bar";
     modeBtn.innerHTML = "🌙";
   }
-  modeBtn.classList.toggle("mode-btn-dark");
-};
+}
